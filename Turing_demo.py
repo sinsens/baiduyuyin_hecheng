99be2fbab8fb19d0939bedba501b631d1e8c4b6c
sinsens <xzapk@hotmail.com>
2017-02-26 21:48:49 +0800

    Turing_demo.py
    图灵机器人Demo，集成百度语音输出

---
+++ Turing_demo.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 26 21:13:41 2017
+
+@author: Sinsen
+@project: Turing Robot demo
+    集成百度语音合成，需要下载同一目录下的baidu_yuyin.py
+"""
+import urllib,json
+from baidu_yuyin import say
+
+uid = '12345'
+def chat(text='hello'):
+    print 'me:',text
+    url = 'http://www.tuling123.com/openapi/api'
+    key = 'af2d922e18944529a9f2ce68d3449027'
+    
+    parmas = urllib.urlencode({
+            'key':key,
+            'info':text,
+            'userid':uid
+            })
+    try:
+        res = urllib.urlopen(url,parmas)
+        res = json.loads(res.read())
+        text = res['text'].encode('utf-8')
+        print 'niko:',text
+        say(text)
+    except:
+        print 'error'
+    
+chat('今天丽江天气怎么样')
